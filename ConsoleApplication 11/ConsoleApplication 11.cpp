1eb5effc3ecd38853067db0f4d9e655291ef7910
demhat yasin <demhatyasin899@gmail.com>
2019-11-24 15:03:35 +0100

    initial commit

---
+++ ConsoleApplication 11/ConsoleApplication 11.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include<cstdlib>
+#include<time.h>
+using namespace std;
+void main()
+{
+	srand(time(NULL));
+	cout << "Zahlenraten Spiel" << endl;
+	int x;
+	x = rand() % 101;	
+	cout << x << endl;
+	for (;;) {
+		int y;
+		cout << "bitte geben sie eine nummer zwichen 0-100 ein :";
+		cin >> y;
+		if (y >= 0 && y <= 100)
+		{
+			if (y == x) {
+				cout << "gratulation du hast die nummer geraten" << endl;
+				break;
+			}
+			else if (y > x)
+			{
+				cout << "nummer größer als die gesuchte zahl" << endl;
+
+			}
+			else
+			{
+				cout << "nummer kleiner als die gesuchte zahl" << endl;
+			}
+		}
+		else {
+			cout << "Ihre zahl nicht moglich geben sie bitte eine nummer zwichen 0-100 ein " << endl;
+		}
+	}
+}
